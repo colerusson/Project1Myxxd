ff75e9f0379032c196a01b85dadf883c55b2c2c8
Cole Russon <106703710+colerusson@users.noreply.github.com>
2022-09-20 14:49:47 -0600

    New Commit

--- main.c
+++ main.c
@@ -146,7 +146,6 @@ void readAndPrintInputAsBits(FILE *input) {
 int main(int argc, char **argv) {
     int bits = FALSE;
     FILE *input = parseCommandLine(argc, argv, &bits);
-    bits = TRUE;
     if (bits == FALSE) {
         readAndPrintInputAsHex(input);
     } else {
